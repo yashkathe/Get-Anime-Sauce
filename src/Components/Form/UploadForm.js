d8b75a781d9674fe63360e9ab405c327529b26b5
Yash Kathe <yashkathe123@gmail.com>
2022-01-12 23:36:16 +0530

    main card layout done with animation

--- src/Components/Form/UploadForm.js
+++ src/Components/Form/UploadForm.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useRef } from "react";
 
 import folderIcon from "../../assets/folder.png";
 import urlIcon from "../../assets/url.png";
@@ -7,6 +7,9 @@ import { motion } from "framer-motion";
 import classes from "./UploadForm.module.css";
 
 const UploadForm = () => {
+    //refs
+
+    const inputFile = useRef(null);
     // variants and hover
 
     const hover = {
@@ -19,6 +22,8 @@ const UploadForm = () => {
 
     const photoHandler = () => {
         setIsUrl(false);
+        inputFile.current.click();
+        console.log(inputFile);
     };
 
     const urlHandler = () => {
@@ -28,7 +33,14 @@ const UploadForm = () => {
     return (
         <React.Fragment>
             <div className={classes.altButtons}>
+                {/* add folder button  */}
+
                 <div>
+                    <input
+                        type='file'
+                        className={classes.fileUpload}
+                        ref={inputFile}
+                    />
                     <motion.button
                         className={`${classes.optButton} ${
                             !isUrl && classes.optButtonActive
@@ -43,6 +55,9 @@ const UploadForm = () => {
                         ></img>
                     </motion.button>
                 </div>
+
+                {/* url insert button  */}
+
                 <div>
                     <motion.button
                         className={`${classes.optButton} ${
@@ -59,18 +74,29 @@ const UploadForm = () => {
                     </motion.button>
                 </div>
             </div>
-            <div>
-                <form>
-                    <input
-                        className={`${classes.urlInput} ${
-                            !isUrl && classes.cancel
-                        }`}
-                        placeholder='Image URL'
-                    />
-                </form>
-                <form>
-                    <input type='file' />
-                </form>
+
+            {/* rest of the form  */}
+
+            <div className={classes.urlDiv}>
+                <motion.input
+                    className={`${classes.urlInput} ${
+                        !isUrl && classes.cancel
+                    }`}
+                    placeholder='Image URL'
+                    whileFocus={{
+                        scale: 1.3,
+                        borderBottom: "3px solid black",
+                        boxShadow: "0 4px 8px rgba(0,0,0,0.5)",
+                        transition: {
+                            type:'tween',
+                            mass:0.8,
+                            duration:0.1
+                        },
+                    }}
+                />
+            </div>
+            <div className={classes.submit}>
+                <button type='submit'>Submit</button>
             </div>
         </React.Fragment>
     );
